7f7f44ff51f782cacc23860299bf7be951f72b10
Aditya <aroo25may@gmail.com>
2025-02-02 20:45:55 +0530

    Header, Footer & Tabs

---
+++ TheWizPoll/src/components/Footer.svelte
@@ -0,0 +1,18 @@
+<footer>
+    <div class="copyright">© 2025 WizPoll. All rights reserved.</div>
+</footer>
+
+<style>
+    footer {
+        padding: 40px;
+        text-align: center;
+    }
+    .copyright {
+        font-size: 14px;
+        color: #aaa;
+        display: inline-block;
+        padding: 20px;
+        border-top: 1px solid #ddd;
+    }
+</style>
+
